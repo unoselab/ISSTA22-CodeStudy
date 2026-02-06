bc036f9db5b97e164eb05009a3af9f084eab88ae
M Song <myoungkyu@nebraska.edu>
2026-02-06 01:25:42 -0600

    2026-02-06 01:25
    Updated

---
+++ Task/Clone-Detection-BigCloneBench/codegpt/run-mix-data.sh
@@ -0,0 +1,8 @@
+python mix_data.py \
+  --train_data_file_bcb ../dataset/train.txt \
+  --valid_data_file_bcb ../dataset/valid.txt \
+  --train_data_file_more ../../../detect_clones/NiCad/post_process/data/java/camel/train.txt \
+  --valid_data_file_more ../../../detect_clones/NiCad/post_process/data/java/camel/valid.txt \
+  --bcb_jsonl ../dataset/data.jsonl \
+  --more_jsonl ../../../detect_clones/NiCad/post_process/data/java/camel/data.jsonl \
+  --seed 3
